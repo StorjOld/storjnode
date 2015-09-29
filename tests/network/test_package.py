1e94c5d05864b6d87acc31a9bd015c5a11da2a27
F483 <fabian.barkhau@gmail.com>
2015-09-29 23:14:02 +0200

    test network package parser drops invalad package

--- tests/network/test_package.py
+++ tests/network/test_package.py
@@ -7,7 +7,7 @@ class TestNetworkPackageParse(unittest.TestCase):
     def test_parse_normal(self):
         ptype = package.PACKAGE_TYPE_DATA
         paddress = b"addraddraddraddraddr"  # shitty btc addr
-        punixtime = (chr(0) * 8).encode("ascii")
+        punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
         pdata_size = (chr(0) + chr(4)).encode("ascii")
         pdata = b"F483"
         psig = b"X" * 65
@@ -16,10 +16,54 @@ class TestNetworkPackageParse(unittest.TestCase):
         expected = {
             "type": ptype,
             "address": paddress,
-            "unixtime": 0,
+            "unixtime": 3,
             "data": pdata,
             "signature": psig
         }
+        self.assertEqual(expected, result)
+
+    def test_ignores_package_to_small(self):
+        result = package.parse(b"")
+        self.assertEqual(None, result)
+
+    def test_ignores_package_to_large(self):
+        result = package.parse(b"X" * 8193)
+        self.assertEqual(None, result)
+
+    def test_ignores_incorrect_type(self):
+        ptype = b"X"
+        paddress = b"addraddraddraddraddr"  # shitty btc addr
+        punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
+        pdata_size = (chr(0) + chr(4)).encode("ascii")
+        pdata = b"F483"
+        psig = b"X" * 65
+        packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
+        result = package.parse(packagedata)
+        expected = None
+        self.assertEqual(expected, result)
+
+    def test_ignores_size_to_small(self):
+        ptype = package.PACKAGE_TYPE_DATA
+        paddress = b"addraddraddraddraddr"  # shitty btc addr
+        punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
+        pdata_size = (chr(0) + chr(3)).encode("ascii")
+        pdata = b"F483"
+        psig = b"X" * 65
+        packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
+        result = package.parse(packagedata)
+        expected = None
+        self.assertEqual(expected, result)
+
+    def test_ignores_size_to_large(self):
+        ptype = package.PACKAGE_TYPE_DATA
+        paddress = b"addraddraddraddraddr"  # shitty btc addr
+        punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
+        pdata_size = (chr(0) + chr(5)).encode("ascii")
+        pdata = b"F483"
+        psig = b"X" * 65
+        packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
+        result = package.parse(packagedata)
+        expected = None
         self.assertEqual(expected, result)
 
 
