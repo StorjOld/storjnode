b4877b9e79bbeea07104c6b52db570dd524734bb
F483 <fabian.barkhau@gmail.com>
2015-09-30 12:43:51 +0200

    completed make and parse network package

--- storjnode/network/package.py
+++ storjnode/network/package.py
@@ -1,15 +1,18 @@
 """
 NETWORK BINARY PACKAGE FORMAT
 1  byte    package type
-20 bytes   sender btcaddress
+21 bytes   sender btcaddress
 8  bytes   unix timestamp
 2  bytes   data size
 x  bytes   data bytes
 65 bytes   signature
 """
 
+import time
 import logging
 import btctxstore
+from pycoin.encoding import b2a_hashed_base58
+from pycoin.encoding import a2b_hashed_base58
 from btctxstore.common import num_to_bytes
 from btctxstore.common import num_from_bytes
 
@@ -18,13 +21,14 @@ log = logging.getLogger(__name__)
 
 
 PACKAGE_TYPES = [b'0', b'1', b'2', b'3']
+PACKAGE_TYPE_NAMES = ["SYN", "SYNACK", "ACK", "DATA"]
 PACKAGE_TYPE_SYN = PACKAGE_TYPES[0]
 PACKAGE_TYPE_SYNACK = PACKAGE_TYPES[1]
 PACKAGE_TYPE_ACK = PACKAGE_TYPES[2]
 PACKAGE_TYPE_DATA = PACKAGE_TYPES[3]
 
 PACKAGE_BYTES_TYPE = 1
-PACKAGE_BYTES_BTCADDRESS = 20  # FIXME is this true?
+PACKAGE_BYTES_BTCADDRESS = 21
 PACKAGE_BYTES_UNIXTIME = 8
 PACKAGE_BYTES_DATASIZE = 2
 PACKAGE_BYTES_SIGNATURE = 65
@@ -38,74 +42,84 @@ PACKAGE_MIN_SIZE = (PACKAGE_BYTES_TYPE + PACKAGE_BYTES_BTCADDRESS
                     + PACKAGE_BYTES_SIGNATURE)
 
 
-def _address_bytes_from_wif(address):
-    pass  # TODO implement
-
-
-def _unixtimestamp_as_bytes():
-    unixtime = 0  # FIXME actually get unixtime
-    return num_to_bytes(PACKAGE_BYTES_UNIXTIME, unixtime)
-
-
-def _sign(signing_wif, data, testnet=False):
-    key = btctxstore.deserialize.key(testnet, signing_wif)
-    return btctxstore.control.sign_data(testnet, data, key)
-
-
 def make(package_type, signing_wif, data_bytes=b"", testnet=False):
+    key = btctxstore.deserialize.key(testnet, signing_wif)
     package = package_type
-    package += _address_bytes_from_wif(signing_wif)
-    package += _unixtimestamp_as_bytes()
+    package += a2b_hashed_base58(key.address())
+    package += num_to_bytes(PACKAGE_BYTES_UNIXTIME, int(time.time()))
     package += num_to_bytes(PACKAGE_BYTES_DATASIZE, len(data_bytes))
     package += data_bytes
-    package += _sign(signing_wif, package, testnet=testnet)
+    package += btctxstore.control.sign_data(testnet, package, key)
     return package
 
 
-def parse(package):
+def parse(package_bytes, dcc_address, expire_time, testnet=False):
 
     # check size
-    if len(package) < PACKAGE_MIN_SIZE:
-        log.warning("Parse error: Package to small!")
+    if len(package_bytes) < PACKAGE_MIN_SIZE:
+        log.warning("Invalid package: Package to small!")
         return None
-    if len(package) > PACKAGE_MAX_SIZE:
-        log.warning("Parse error: Package to large!")
+    if len(package_bytes) > PACKAGE_MAX_SIZE:
+        log.warning("Invalid package: Package to large!")
         return None
-    package_stack = package[:]  # copy it
+    stack = package_bytes[:]  # copy and use like a stack
 
     # parse type
-    package_type = package_stack[:PACKAGE_BYTES_TYPE]
-    package_stack = package_stack[PACKAGE_BYTES_TYPE:]  # pop type
+    package_type = stack[:PACKAGE_BYTES_TYPE]
+    stack = stack[PACKAGE_BYTES_TYPE:]  # pop type
     if package_type not in PACKAGE_TYPES:
-        log.warning("Parse error: Invalid package type!")
+        log.warning("Invalid package: Invalid package type!")
         return None
 
     # parse address
-    address_bytes = package_stack[:PACKAGE_BYTES_BTCADDRESS]
-    package_stack = package_stack[PACKAGE_BYTES_BTCADDRESS:]  # pop address
-    address = address_bytes  # FIXME actually convert
-
-    # get timestamp
-    unixtime_bytes = package_stack[:PACKAGE_BYTES_UNIXTIME]
-    package_stack = package_stack[PACKAGE_BYTES_UNIXTIME:]  # pop unixtime
-    unixtime = num_from_bytes(PACKAGE_BYTES_UNIXTIME, unixtime_bytes)
-
-    # get data size
-    data_size_bytes = package_stack[:PACKAGE_BYTES_DATASIZE]
-    package_stack = package_stack[PACKAGE_BYTES_DATASIZE:]  # pop data size
-    data_size = num_from_bytes(PACKAGE_BYTES_DATASIZE, data_size_bytes)
-    if len(package_stack) != (data_size + PACKAGE_BYTES_SIGNATURE):
-        log.warning("Parse error: Invalid data size!")
+    address_bytes = stack[:PACKAGE_BYTES_BTCADDRESS]
+    stack = stack[PACKAGE_BYTES_BTCADDRESS:]  # pop address
+    address = b2a_hashed_base58(address_bytes)
+    if address != dcc_address:
+        logmsg = "Invalid package: Package address {0} != dcc address {1}!"
+        log.warning(logmsg.format(address, dcc_address))
         return None
 
-    # get data and signature
-    data_bytes = package_stack[:data_size]
-    signature = package_stack[data_size:]  # pop data
+    # get timestamp
+    unixtime_bytes = stack[:PACKAGE_BYTES_UNIXTIME]
+    stack = stack[PACKAGE_BYTES_UNIXTIME:]  # pop unixtime
+    package_unixtime = num_from_bytes(PACKAGE_BYTES_UNIXTIME, unixtime_bytes)
+    current_unixtime = int(time.time())
+    time_delta = current_unixtime - package_unixtime
+    if abs(time_delta) > expire_time:
+        logmsg = "Invalid package: Stale package abs({0}) > {1}!"
+        log.warning(logmsg.format(time_delta, expire_time))
+        return None
+
+    # get data size
+    data_size_bytes = stack[:PACKAGE_BYTES_DATASIZE]
+    stack = stack[PACKAGE_BYTES_DATASIZE:]  # pop data size
+    data_size = num_from_bytes(PACKAGE_BYTES_DATASIZE, data_size_bytes)
+    if len(stack) != (data_size + PACKAGE_BYTES_SIGNATURE):
+        log.warning("Invalid package: Invalid data size {0}!".format(data_size))
+        return None
+    if data_size > 0 and package_type != PACKAGE_TYPE_DATA:
+        logmsg = "Invalid package: {0}bytes data for non data package!"
+        log.warning(logmsg.format(data_size))
+        return None
+
+    # get data
+    data_bytes = stack[:data_size]
+    stack = stack[data_size:]  # pop data
+
+    # verify signature
+    sig = package_bytes[-PACKAGE_BYTES_SIGNATURE:] # signature data
+    signed = package_bytes[:-PACKAGE_BYTES_SIGNATURE]  # signed data
+    assert(sig == stack)  # only signature should be left on the stack
+    if not btctxstore.control.verify_signature(testnet, address, sig, signed):
+        log.warning("Invalid package: bad signature!")
+        return None
 
     return {
         "type": package_type,
         "address": address,
-        "unixtime": unixtime,
+        "unixtime": package_unixtime,
         "data": data_bytes,
-        "signature": signature
+        "signature": sig
     }
+
--- tests/network/test_package.py
+++ tests/network/test_package.py
@@ -1,33 +1,39 @@
+import logging
+LOG_FORMAT = "%(levelname)s %(name)s %(lineno)d: %(message)s"
+logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
+
+
 import unittest
+from btctxstore import BtcTxStore
 from storjnode.network import package
+from pycoin.encoding import b2a_hashed_base58
+from pycoin.encoding import a2b_hashed_base58
+
+
 
 
 class TestNetworkPackageParse(unittest.TestCase):
 
+    def setUp(self):
+        self.btctxstore = BtcTxStore()
+        self.wif = self.btctxstore.create_key()
+        self.address = self.btctxstore.get_address(self.wif)
+
     def test_parse_normal(self):
-        ptype = package.PACKAGE_TYPE_DATA
-        paddress = b"addraddraddraddraddr"  # shitty btc addr
-        punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
-        pdata_size = (chr(0) + chr(4)).encode("ascii")
-        pdata = b"F483"
-        psig = b"X" * 65
-        packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
-        result = package.parse(packagedata)
-        expected = {
-            "type": ptype,
-            "address": paddress,
-            "unixtime": 3,
-            "data": pdata,
-            "signature": psig
-        }
-        self.assertEqual(expected, result)
+        package_bytes = package.make(package.PACKAGE_TYPE_DATA, self.wif, 
+                                     b"F483", self.btctxstore.testnet)
+        self.assertTrue(package_bytes != None)
+
+        parsed = package.parse(package_bytes, self.address, 2, 
+                               self.btctxstore.testnet)
+        self.assertTrue(parsed != None)
 
     def test_ignores_package_to_small(self):
-        result = package.parse(b"")
+        result = package.parse(b"", None, 2)
         self.assertEqual(None, result)
 
     def test_ignores_package_to_large(self):
-        result = package.parse(b"X" * 8193)
+        result = package.parse(b"X" * 8193, None, 2)
         self.assertEqual(None, result)
 
     def test_ignores_incorrect_type(self):
@@ -38,7 +44,7 @@ class TestNetworkPackageParse(unittest.TestCase):
         pdata = b"F483"
         psig = b"X" * 65
         packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
-        result = package.parse(packagedata)
+        result = package.parse(packagedata, paddress, 2)
         expected = None
         self.assertEqual(expected, result)
 
@@ -50,19 +56,19 @@ class TestNetworkPackageParse(unittest.TestCase):
         pdata = b"F483"
         psig = b"X" * 65
         packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
-        result = package.parse(packagedata)
+        result = package.parse(packagedata, paddress, 2)
         expected = None
         self.assertEqual(expected, result)
 
     def test_ignores_size_to_large(self):
         ptype = package.PACKAGE_TYPE_DATA
-        paddress = b"addraddraddraddraddr"  # shitty btc addr
+        paddress = b"addraddraddraddraddra"  # shitty btc addr
         punixtime = (chr(0) * 7 + chr(3)).encode("ascii")
         pdata_size = (chr(0) + chr(5)).encode("ascii")
         pdata = b"F483"
         psig = b"X" * 65
         packagedata = ptype + paddress + punixtime + pdata_size + pdata + psig
-        result = package.parse(packagedata)
+        result = package.parse(packagedata, paddress, 2)
         expected = None
         self.assertEqual(expected, result)
 
