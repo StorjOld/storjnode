a7e95a5bc49e8e85639c858ad7b7bf1b993bbd87
F483 <fabian.barkhau@gmail.com>
2015-10-05 18:56:37 +0200

    test network receiver requires hdwallet (to use storj config wallets)

--- storjnode/bin/test_network_receiver.py
+++ storjnode/bin/test_network_receiver.py
@@ -25,8 +25,8 @@ def get_args():
             sys.exit(2)
     description = "Dataserve client command-line interface."
     parser = ArgumentParser(description=description)
-    parser.add_argument("wallet", help="Bitcoin WIF is this nodes identity"
-                                       " and for signing.")
+    parser.add_argument("hdwallet", help="Bitcoin HWIF is this nodes identity"
+                                         " and for signing.")
     parser.add_argument("--storepath" , default=STOREPATH,
                         help="Path to store data. default=%s" % STOREPATH)
     return vars(parser.parse_args())
@@ -34,14 +34,17 @@ def get_args():
 
 if __name__ == "__main__":
     arguments = get_args()
-    service = network.Service(RELAYNODES, arguments["wallet"])
+    hdwallet = arguments["hdwallet"]
+    key = btctxstore.get_key(hdwallet)
+    service = network.Service(RELAYNODES, key)
     try:
         service.connect()
         while True:
             received = service.get_received()
             for node, data in received.items():
                 # FIXME make storepath dirs
-                path = os.path.realpath(os.path.join(arguments["storepath"], node))
+                storepath = arguments["storepath"]
+                path = os.path.realpath(os.path.join(storepath, node))
                 with open(path, "ab") as f:
                     f.write(data)
             time.sleep(0.1)
