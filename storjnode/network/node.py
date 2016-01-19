0cda31a4d631f26a31f68fe097861e4d91799cf0
Matthew Roberts <matthew@roberts.pm>
2016-01-19 16:03:22 -0500

    Try fix travis ...

--- storjnode/network/node.py
+++ storjnode/network/node.py
@@ -17,15 +17,18 @@ from pyp2p.unl import UNL
 # File transfer.
 from storjnode.network.file_transfer import FileTransfer
 from storjnode.network.process_transfers import process_transfers
+from storjnode.network.process_transfers import HANDSHAKE_TIMEOUT
+from storjnode.network.process_transfers import CON_TIMEOUT
 from storjnode.network.bandwidth.test import BandwidthTest
 from storjnode.network.bandwidth.limit import BandwidthLimit
+import storjnode.network
 from pyp2p.net import Net
 
 
 _log = storjnode.log.getLogger(__name__)
 
-storjnode.network.process_transfers.HANDSHAKE_TIMEOUT = 100000000000
-storjnode.network.process_transfers.CON_TIMEOUT = 100000000000
+HANDSHAKE_TIMEOUT = 100000000000
+CON_TIMEOUT = 100000000000
 
 
 DEFAULT_BOOTSTRAP_NODES = [
