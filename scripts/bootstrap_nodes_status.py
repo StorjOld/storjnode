58129b5d275871d819d54c38a67c2c1ae937cc09
F483 <fabian.barkhau@gmail.com>
2016-01-20 16:41:13 -0500

    added bootstrap node status script

--- scripts/bootstrap_nodes_status.py
+++ scripts/bootstrap_nodes_status.py
@@ -7,4 +7,87 @@ import os
 os.environ["PYCOIN_NATIVE"] = "openssl"
 
 
-# TODO implement
+import json  # NOQA
+import argparse  # NOQA
+import signal  # NOQA
+import storjnode  # NOQA
+import btctxstore  # NOQA
+from twisted.internet import defer  # NOQA
+from crochet import setup  # NOQA
+from storjnode.common import TESTGROUPB_BOOTSTRAP_NODES  # NOQA
+from storjnode.common import TESTGROUPC_BOOTSTRAP_NODES  # NOQA
+from kademlia.node import Node  # NOQA
+from crochet import wait_for  # NOQA
+
+
+# start twisted via crochet and remove twisted handler
+setup()
+signal.signal(signal.SIGINT, signal.default_int_handler)
+
+
+_log = storjnode.log.getLogger(__name__)
+
+
+def parse_args():
+    description = "Start a storjnode bootstrap only node."
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--quiet', action='store_true')
+    parser.add_argument('--testgroupb', action='store_true',
+                        help="Node for testgroupb.")
+    return vars(parser.parse_args())
+
+
+def make_config(bootstrap_nodes):
+    config = storjnode.config.create()
+    config["network"]["bootstrap_nodes"] = bootstrap_nodes
+    config["network"]["disable_data_transfer"] = True
+    config["network"]["monitor"]["enable_crawler"] = False
+    config["network"]["monitor"]["enable_responses"] = False
+    storjnode.config.validate(config)
+    return config
+
+
+@wait_for(timeout=60)
+def get_bootstrap_nodes_status(node, bootstrap_nodes):
+
+    def on_success(results):
+        _log.info("RESULTS: {0}".format(repr(results)))
+        assert(len(results) == len(bootstrap_nodes))
+        status = []
+        for index in range(len(results)):
+            success, ourid = results[index]
+            ip, port = bootstrap_nodes[index]
+            status.append([[ip, port], "online" if success else "offline"])
+        return status
+
+    ds = []
+    for ip, port in bootstrap_nodes:
+        knode = Node(b"X"*20, ip=ip, port=port)
+        ds.append(node.server.protocol.callPing(knode))
+    return defer.gatherResults(ds).addCallback(on_success)
+
+
+def main():
+    args = parse_args()
+    if args["testgroupb"]:
+        bootstrap_nodes = TESTGROUPB_BOOTSTRAP_NODES
+    else:
+        bootstrap_nodes = TESTGROUPC_BOOTSTRAP_NODES
+    config = make_config(bootstrap_nodes)
+    wallet = btctxstore.BtcTxStore().create_wallet()
+    node = None
+    try:
+        node = storjnode.network.Node(wallet, config=config)
+        status = get_bootstrap_nodes_status(node, bootstrap_nodes)
+        print(json.dumps(status, indent=2))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if node is not None:
+            node.stop()
+
+
+if __name__ == "__main__":
+    main()
--- scripts/start_bootstrap_only_node.py
+++ scripts/start_bootstrap_only_node.py
@@ -12,6 +12,7 @@ import signal  # NOQA
 import storjnode  # NOQA
 import btctxstore  # NOQA
 from crochet import setup  # NOQA
+from storjnode.common import TESTGROUPB_BOOTSTRAP_NODES  # NOQA
 
 
 # start twisted via crochet and remove twisted handler
@@ -20,17 +21,21 @@ signal.signal(signal.SIGINT, signal.default_int_handler)
 
 
 def parse_args():
-    description = "Start a storjnode bootstrap node."
+    description = "Start a storjnode bootstrap only node."
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--port", type=int, help="UDP port to listen on.")
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--verbose', action='store_true')
     parser.add_argument('--quiet', action='store_true')
+    parser.add_argument("--port", type=int, help="UDP port to listen on.")
+    parser.add_argument('--testgroupb', action='store_true',
+                        help="Node for testgroupb.")
     return vars(parser.parse_args())
 
 
-def make_config(port):
+def make_config(port, testgroupb):
     config = storjnode.config.create()
+    if testgroupb:
+        config["network"]["bootstrap_nodes"] = TESTGROUPB_BOOTSTRAP_NODES
     config["network"]["port"] = port
     config["network"]["disable_data_transfer"] = True
     config["network"]["monitor"]["enable_crawler"] = False
@@ -41,10 +46,17 @@ def make_config(port):
 
 def main():
     args = parse_args()
-    config = make_config(args["port"])
+    config = make_config(args["port"], args["testgroupb"])
     wallet = btctxstore.BtcTxStore().create_wallet()
-    node = storjnode.api.StorjNode(wallet=wallet, config=config)
-    node.farm()
+    node = None
+    try:
+        node = storjnode.api.StorjNode(wallet=wallet, config=config)
+        node.farm()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if node is not None:
+            node.on_shutdown()
 
 
 if __name__ == "__main__":
