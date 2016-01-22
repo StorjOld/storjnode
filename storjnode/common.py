e02273b3f9d55395b22da088b6dd1139e16a62cb
F483 <fabian.barkhau@gmail.com>
2016-01-22 12:52:40 -0500

    pep8 fix

--- storjnode/common.py
+++ storjnode/common.py
@@ -9,7 +9,6 @@ MAX_PACKAGE_DATA = 548  # 576 MTU - 20 IPv4 Header - 8 UDP Header == 548
 STORJ_HOME = os.path.join(os.path.expanduser("~"), ".storj")
 CONFIG_PATH = os.path.join(STORJ_HOME, "cfg.json")
 
-
 DEFAULT_BOOTSTRAP_NODES = [
     ["104.236.1.59", 4653],     # storj stable
     ["104.236.1.59", 59744],    # storj stable
