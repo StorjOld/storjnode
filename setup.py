d79ea58f7d74a610c7a1f6baae997117453d8678
littleskunk <jens.heimbuerge@googlemail.com>
2016-01-23 18:25:57 +0100

    workaround py2exe psutil python2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if os.name == 'nt' and 'py2exe' in sys.argv:
     import py2exe  # NOQA
     options = {'py2exe': {
         "optimize": 2,
-        "bundle_files": 2,  # This tells py2exe to bundle everything
+        "bundle_files": 3,  # This tells py2exe to bundle everything
     }}
 
 
