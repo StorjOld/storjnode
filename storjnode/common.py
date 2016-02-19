014caebb0fd0392037ed607923954c1b44bde893
F483 <fabian.barkhau@gmail.com>
2016-02-19 19:04:04 +0100

    added new bootstrapnodes

--- storjnode/common.py
+++ storjnode/common.py
@@ -10,8 +10,8 @@ STORJ_HOME = os.path.join(os.path.expanduser("~"), ".storj")
 CONFIG_PATH = os.path.join(STORJ_HOME, "cfg.json")
 
 DEFAULT_BOOTSTRAP_NODES = [
-    ["159.203.150.90", 10000],   # storj_useast01
     # TODO storj_uswest01
-    # TODO storj_eu01
-    # TODO storj_asia01
+    ["159.203.150.90", 10000],   # storj_useast01
+    ["46.101.71.172", 10000],    # storj_eu01
+    ["188.166.207.187", 10000],  # storj_asia01
 ]
